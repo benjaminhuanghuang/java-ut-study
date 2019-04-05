7c3c0f23bec01dcb27a5d8fc0d249e4eeb0bbea6
Benjamin Huang <bhuang@rms.com>
2019-04-04 21:34:28 -0700

    add sample

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Reference
 - Unit Testing and Coverage in IntelliJ IDEA
 - Using Maven in IntelliJ IDEA
+- [practical-test-driven-development-for-java-programmers] (https://www.linkedin.com/learning/practical-test-driven-development-for-java-programmers/)
+
+
+## Rules of TDD
+- Test the expected outcome of an example
+
+- Don't pre-judge design... let your tests drive it.
+
+- Write the minimum code required to get your tests to pass
+
+- Each test should validate one single piece of logic
+
---
+++ src/test/java/cn/huang/isbntools/ISBNValidatorTest.java
@@ -0,0 +1,60 @@
+package cn.huang.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ISBNValidatorTest {
+
+    public void checkAValid10DigitISBN() {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAValid13DigitISBN() {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("9781853260087");
+        assertTrue("first value", result);
+        result = validator.checkISBN("9781853267338");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void TenDigitISBNNumbersEndingInAnXAreValid() {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitISBN() {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitISBN() {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("9781853267336");
+        assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nineDigitISBNsAreNotAllowed() {
+        ISBNValidator validator = new ISBNValidator();
+        validator.checkISBN("123456789");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nonNumericISBNsAreNotAllowed() {
+        ISBNValidator validator = new ISBNValidator();
+        validator.checkISBN("helloworld");
+    }
+
+
+}
